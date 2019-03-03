0521be620b9c01705014c327a54f59011c03745f
tcmryan <ryan@tacticalcodemonkeys.com>
2019-03-03 14:55:24 -0500

    remove overwrite and repeat options, add in usage reports

--- lib/actions.py
+++ lib/actions.py
@@ -1,9 +1,11 @@
 import csv
 import os
+import xlsxwriter
 
+from setting import config
 from .dojo_requests import DojoRequests
 from .grading_page import GradingPage
-from .belt_scripts import fetch_students
+from .belt_scripts import get_ninjas_info, get_get_ninjas_usage
 
 
 def get_grading_page(session, search_term='', page_num=1, page_size=100, include_graded='true'):
@@ -18,8 +20,10 @@ def get_grading_page(session, search_term='', page_num=1, page_size=100, include
 
 
 def create_csv(headers, data, filename='grades.csv'):
-    if os.path.exists(filename):
-        pass
+    # if os.path.exists(filename) and not config['file_config']['overwrite']:
+    #     name, _ = filename.split('.csv')
+    #     now = datetime.datetime.now()
+    #     filename = name + now.strftime('%d%B%y-%H:%M') + '.csv'
 
     with open(filename, 'w', newline='') as f:
         w = csv.DictWriter(f, headers)
@@ -44,6 +48,45 @@ def save_grades(filename, search_term=''):
 
 
 def save_belts(filename, site_name):
-    students = fetch_students(site_name)
+    students = get_ninjas_info(site_name)
     headers = students[0].keys()
     create_csv(headers, students, filename)
+
+
+def save_usage(filename, site_name, excel_fmt=False):
+    """
+    Creates a CSV for each month.  If excel mode selected, it will create 1 workbook
+    with a sheet per month
+    :param filename:
+    :param site_name:
+    :param excel_fmt: boolean to write in excel format
+    :return:
+    """
+    usages = get_get_ninjas_usage(site_name)
+    # if excel_fmt:
+    #     if os.path.exists(filename):
+    #         os.remove(filename)
+    #     wb = xlsxwriter.Workbook(filename)
+    #     for key, value in usages.items():
+    #         write_excel_ws(wb, key, value[0].keys(), value)
+    headers = usages[0].keys()
+    create_csv(headers, usages, filename=filename)
+
+
+def scheduled_work():
+    if config['file_config'].get('repeat'):
+        save_grades(config['file_config']['name'], config['search_term'])
+
+
+def write_excel_ws(workbook, name, headers, data):
+    ws = workbook.add_worksheet(name)
+    write_row(ws, 0, headers)
+    for i, row in enumerate(data):
+        row_num = i + 1  # Account for the headers
+        write_row(ws, row_num, data)
+
+
+def write_row(worksheet, row, data):
+    for i, header in enumerate(data):
+        worksheet.write(row, i, data)
+
--- setting.py
+++ setting.py
@@ -4,6 +4,10 @@ import os
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 CONFIG_FILE = os.path.join(ROOT_DIR, 'config.yml')
 
+SAVED_GRADES_KEY = 'saved_grades'
+SAVED_BELTS_KEY = 'saved_belts'
+SAVED_USAGES_KEY = 'saved_usages'
+
 
 def load_settings():
     with open(CONFIG_FILE, 'r') as ymlfile:
